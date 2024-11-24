7173ab0d0da5b0da5fa0b5ac6f13437f66f36a75
Joshua Shew <joshua.t.shew@gmail.com>
2024-11-24 17:54:46 -0500

    Maintainance changes
    - Updated script names and object notes
    - Combined calculate_snapshot_timing and check_if_valid_snapshot
    - Removed the outdated logging configuration option
    - Updated the "use website" option to skip checks for existing snapshots

--- Workflow/src/check_if_valid_snapshot.sh
+++ Workflow/src/check_if_valid_snapshot.sh
@@ -1,7 +1,17 @@
 #!/bin/zsh --no-rcs
 
-# $1 is assume to be the snapshot tolerance in seconds
-if (( $1 >= $seconds_since_snapshot )); then
+# Input timestamp (YYYYMMDDHHMMSS)
+input_timestamp="${snapshot_timestamp_raw}"
+input_epoch=$(TZ=UTC date -j -f "%Y%m%d%H%M%S" "${input_timestamp}" "+%s")
+current_epoch=$(TZ=UTC date "+%s")
+seconds_since_snapshot=$((current_epoch - input_epoch))
+
+# Compare with the snapshot tolerance in seconds
+snapshot_tolerance_days="${snapshot_tolerance}"
+snapshot_tolerance_seconds=$((snapshot_tolerance_days * 86400))
+
+# Determine if the snapshot is still valid
+if (( snapshot_tolerance_seconds >= $seconds_since_snapshot )); then
   echo -n 1
 else
   echo -n 0
